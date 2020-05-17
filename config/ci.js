adca996ceef363202b73a45177cf7fe10fb7324c
Sumit Govil <sumit.govil@optus.com.au>
2020-05-17 19:00:37 +1000

    CI configuration

--- config/ci.js
+++ config/ci.js
@@ -2,8 +2,7 @@ module.exports = {
   googleClientID:
     "869557452064-p8q21jcqf2vecbrah14koh7ttfjesd1a.apps.googleusercontent.com",
   googleClientSecret: "4JdF44XaCbwmWphqcGVY-5aY",
-  mongoURI:
-    "mongodb+srv://sumit:sumit@freecluster-nrn0d.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379",
 };
--- tests/header.test.js
+++ tests/header.test.js
@@ -5,7 +5,7 @@ let page;
 describe("Header", () => {
   beforeEach(async () => {
     page = await Page.build();
-    await page.goto("localhost:3000");
+    await page.goto("http://localhost:3000");
   });
 
   afterEach(async () => {
