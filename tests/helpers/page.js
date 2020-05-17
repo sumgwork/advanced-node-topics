cfcfb04d8486f1f6dc9f3ef45adcc89ed9f15b63
Sumit Govil <sumit.govil@optus.com.au>
2020-05-18 09:10:22 +1000

    renamed again

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const sessionFactory = require("../factories/sessionFactory");
 const userFactory = require("../factories/userFactory");
 
-class CustomPage {
+class Page {
   static async build() {
     const browser = await puppeteer.launch({
       headless: true,
@@ -10,7 +10,7 @@ class CustomPage {
     });
 
     const page = await browser.newPage();
-    const customPage = new CustomPage(page);
+    const customPage = new Page(page);
 
     return new Proxy(customPage, {
       get: function (target, property) {
@@ -40,4 +40,4 @@ class CustomPage {
   }
 }
 
-module.exports = CustomPage;
+module.exports = Page;
